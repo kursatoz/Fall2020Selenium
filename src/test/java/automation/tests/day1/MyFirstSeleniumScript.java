c029c2109377b3059f65249f1c427ea7eec3bf76
kurshatoz@gmail.com <kurshatoz@gmail.com>
2020-03-02 16:06:58 -0500

    added new code

--- src/test/java/automation/tests/day1/MyFirstSeleniumScript.java
+++ src/test/java/automation/tests/day1/MyFirstSeleniumScript.java
@@ -3,15 +3,19 @@ package automation.tests.day1;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.RemoteWebDriverBuilder;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class MyFirstSeleniumScript {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver =new ChromeDriver();
-        driver.get("http://google.com");
 
+        driver.get("http://google.com");
+        Thread.sleep(2000);
         driver.close();
     }
 }
